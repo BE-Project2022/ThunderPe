0771459acd9b71906dc0c6037cc8127279a18a37
Rohit <rohitkul247@gmail.com>
2022-01-22 11:36:29 +0530

    added sh file

---
+++ server/something.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+export JWT_SECRET=wearebeprojectmembersand&buildingafull#fledged$#reactnativemobile!paymentsappusingnotusingweb69;
+export MONGODB_URI=mongodb+srv://Group18:Group18@cluster0.wsx5d.mongodb.net/ThunderUser?retryWrites=true&w=majority;
