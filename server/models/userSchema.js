4a3cc5d0120228905ffe4df8de4718ac1b22a620
pratik-agarwal03 <pratik0312@gmail.com>
2022-02-01 17:25:34 +0530

    ✅ Pin key added

--- server/models/userSchema.js
+++ server/models/userSchema.js
@@ -5,6 +5,7 @@ const userSchema = new mongoose.Schema({
   email: { type: String, unique: true },
   password: String,
   mobile: { type: Number, unique: true },
+  pin: { type: Number, maxlength: 4 }
 });
 
 const ThunderUser = new mongoose.model("ThunderUser", userSchema);
