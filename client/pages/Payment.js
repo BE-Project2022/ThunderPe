bbb41d3580f7720e2e01af4d66d879eff1f40d3a
Rohit <rohitkul247@gmail.com>
2022-02-19 09:56:27 +0530

    added payment sound

--- client/pages/Payment.js
+++ client/pages/Payment.js
@@ -5,49 +5,66 @@ import { light, dark } from "../controllers/Theme";
 import LottieView from "lottie-react-native";
 import jwtDecode from "jwt-decode";
 import axios from "axios";
+import { Audio } from "expo-av";
 
 const Payment = ({ navigation, route }) => {
   const from = route.params.from;
   const to = route.params.to;
 
-  const success = false;
+  const success = true;
   // setTimeout(() => {
 
   //   navigation.navigate("Dashboard");
   // }, 5000);
+
+  async function playSound() {
+    console.log("Loading Sound");
+    const { sound } = await Audio.Sound.createAsync(
+      require("../assets/sounds/thunderpayment.mp3")
+    );
+    // setSound(sound);
+
+    console.log("Playing Sound");
+    await sound.playAsync();
+  }
+
+  useEffect(() => {
+    playSound();
+  }, []);
+
   return (
     <View>
       {success
         ? [
-          <View key={0}>
-            <View style={styles.header}></View>
-            <View style={{ alignItems: "center" }}>
-              <Image source={Logo} style={styles.img} />
-            </View>
-            <LottieView
-              style={styles.success}
-              source={require("../assets/images/success.json")}
-              autoPlay
-              loop
-            />
-            <Text style={styles.bottom}>Payment Successful</Text>
-          </View>,
-        ]
+            <View key={0}>
+              <View style={styles.header}></View>
+              <View style={{ alignItems: "center" }}>
+                <Image source={Logo} style={styles.img} />
+              </View>
+              <LottieView
+                style={styles.success}
+                source={require("../assets/images/success.json")}
+                autoPlay
+                loop
+              />
+              <Text style={styles.bottom}>Payment Successful</Text>
+            </View>,
+          ]
         : [
-          <View key={1}>
-            <View style={styles.header}></View>
-            <View style={{ alignItems: "center" }}>
-              <Image source={Logo} style={styles.img} />
-            </View>
-            <LottieView
-              style={styles.success}
-              source={require("../assets/images/failure.json")}
-              autoPlay
-              loop
-            />
-            <Text style={styles.bottom}>Payment Failed</Text>
-          </View>,
-        ]}
+            <View key={1}>
+              <View style={styles.header}></View>
+              <View style={{ alignItems: "center" }}>
+                <Image source={Logo} style={styles.img} />
+              </View>
+              <LottieView
+                style={styles.success}
+                source={require("../assets/images/failure.json")}
+                autoPlay
+                loop
+              />
+              <Text style={styles.bottom}>Payment Failed</Text>
+            </View>,
+          ]}
     </View>
   );
 };
