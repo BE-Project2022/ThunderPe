5c2fa0652786fd9c58333da2a945d4f169eb98f7
pratik-agarwal03 <pratik0312@gmail.com>
2022-02-20 15:22:02 +0530

    token creation fields updated

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -52,6 +52,7 @@ export const login = async (req, res) => {
           id: user._id,
           pin: user.pin,
           name: user.fullname,
+          image: user.image
         },
         process.env.JWT_SECRET,
         { expiresIn: "1h" }
