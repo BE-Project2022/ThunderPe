a7795ee06b1f810918992ca3a08acc000dfe0a64
rutuja <dhanawaderutuja1002@gmail.com>
2022-03-24 20:30:52 +0530

    Contract Events Updated

---
+++ contracts/owned.sol
@@ -0,0 +1,25 @@
+pragma solidity 0.8.6;
+
+// Defining owner nd ownership.
+contract Owned {
+    address public owner; // from
+    address public newOwner;  // to
+
+    event OwnershipTransferred(address indexed _from, address indexed _to);
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function transferOwnership(address _to) public {
+        require(msg.sender == owner);
+        newOwner = _to;
+    }
+
+    function acceptOwnership() public {
+        require(msg.sender == newOwner);
+        emit OwnershipTransferred(owner, newOwner);
+        owner = newOwner;
+        newOwner = address(0);
+    }
+}
